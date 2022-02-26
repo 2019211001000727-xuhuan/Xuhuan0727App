b049f72266720e966cb2afcd82f5274c1c7509a7
2019211001000727-xuhuan <245908199@qq.com>
2022-02-26 13:22:31 +0800

    添加项目文件。

---
+++ W1HW3App/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1HW3App
+{
+    internal class LeapYear
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter year");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                Console.WriteLine("{0} is a leap year", year);
+
+            }
+            else
+                Console.WriteLine("{0} is not a leap year", year);
+            Console.ReadKey();
+        }
+    }
+}
